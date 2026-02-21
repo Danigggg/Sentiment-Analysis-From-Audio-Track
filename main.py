8da136c6c8771f62d71f2982cb91b10bc2c5e8bc
Daniele Mattei <danielemattei64@gmail.com>
2026-02-21 15:12:06 +0100

    Test tensori

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import torch
 
-print(torch.__version__)
+tensor1 = torch.randn(3,4)
+print(tensor1)
+
+tensor2 = torch.randn(4)
+print(tensor2)
+
+result = torch.matmul(tensor1,tensor2)
+
+print(result)
